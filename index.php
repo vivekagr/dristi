75547bd2e2f12a94da6c0f67583aecd39f40e095
Vivek Agarwal <me@vivek.im>
2013-12-18 01:59:47 +0530

    Fix filter (grayscale) in Firefox.

--- index.php
+++ index.php
@@ -37,12 +37,6 @@
             </div>
         </div>
     </div>
-
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <filter id="grayscale">
-            <feColorMatrix type="matrix" values="0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0"/>
-        </filter>
-    </svg>
     <div class="feature-box-container">
         <div class="row">
             <div class="col-sm-4 feature-box">
