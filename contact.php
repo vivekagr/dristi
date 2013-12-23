3f14155e2a20674dfa8db6c96b4dadc427db25cc
Vivek Agarwal <me@vivek.im>
2013-12-23 10:39:57 +0530

    Fix a bug in email sender.

--- contact.php
+++ contact.php
@@ -7,14 +7,16 @@ require_once 'vars.php';
 if ($_POST['message']) {
     $message = "Message received from contact form.\r\n\r\n\r\n
     Name: " . $_POST['name'] . "\r\n\r\nEmail: " . $_POST['email']. "\r\n\r\nMessage: " . $_POST['message'];
+    $subject = $_POST['subject'];
 } else {
     $message = "Message received from volunteer form.\r\n\r\n\r\n
     Name: " . $_POST['name'] . "\r\n\r\nEmail: " . $_POST['email'] . "\r\n\r\nNumber: " . $_POST['mobile'];
+    $subject = "Message From Volunteer Form";
 }
 
 $header = "From: " . $_POST['email'];
 
-$retval = mail ($contactEmail, $_POST['subject'], $message, $header);
+$retval = mail ($contactEmail, $subject, $message, $header);
 
 if ($retval === true) {
     echo "Message sent successfully!";
