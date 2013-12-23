07a7ae9c069f31cfa1a1470d75436afcfe417669
Vivek Agarwal <me@vivek.im>
2013-12-23 05:42:00 +0530

    Add events page.

---
+++ events.php
@@ -0,0 +1,51 @@
+<?php include 'base.php' ?>
+
+<?php startblock('title') ?>Dristi - Events<?php endblock() ?>
+
+<?php startblock('main') ?>
+<div class="hero-container project-container">
+    <div class="row">
+        <div class="col-md-12 full-width-block">
+            <h1 class="heading-text">Events</h1>
+            <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-text-green">Football Match</h2></div>
+                <div class="col-lg-7 col-md-6">
+                    <p>It took no time for team DRISTI to perceive their passion for the game and in no time it was decided to conduct a first of its kind match at our own sports field between the girls of the two villages. The match turned out to be a huge success with the honorary presence of Dr. Jeeoot Singh (DRISTI advisor and professor of Mechanical Department), Dr, Arvin Kumar (HoD, Department of Mechanical Engineering) and Dr. Atul Anand (senior professor, Department of Production Engineering). The first ever DRISTI cup was held up high by the girls of Team Panchauli following an enthralling 1-0 win over Team Masu. DRISTI has decided to conduct such matches, especially for the girls to encourage young talent to take up the game and make a career in it. And very soon we will be having our own football club, F.C. DRISTI. The structuring is in progress and the players will be selected from Masu and Panchauli. Proper training will be imparted to all the players on a regular basis.</p>
+                </div>
+                <div class="col-lg-5 col-md-6"><img class="img-responsive" src="/img/events/football.jpg"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-text-green">Book Donation Drive</h2></div>
+                <div class="col-lg-5 col-md-6"><img class="img-responsive" src="/img/events/book-donation.jpg"></div>
+                <div class="col-lg-7 col-md-6">
+                    <p>Having access to books is the key to literacy, yet the majority of children in low-income neighborhoods often lack libraries and bookstores. While some are fortunate enough to buy books, some of the less fortunate even struggle to find a study material. People like us must be having books which are of no use to us anymore ad are simply collecting dust. So, team DRISTI started this donation scheme and appealed to everyone to donate their books. The book donation scheme of DRISTI is named as "Arun Siksha Protsahan Yojna" in respect of late Mr M. Arun Kumar of 2K9 batch. All the books collected till are kept in the institute premises under the supervision of Dr. Jeeoot Singh.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-text-green">Photojournalism</h2></div>
+                <div class="col-lg-7 col-md-6">
+                    <p>This event was conducted by DRISTI in BIT's annual techno-cultural fest BITOTSAV 2013 to create awareness, among the 5000 students studying here and in other colleges about the problems faced by the majority of rural population in India. There are many people willing to work for some social cause and contribute their bit to society. DRISTI provides a platform for these people and photojournalism was a great way to understand the image of rural India, our young college students have in their minds. It provoked them to think about the under-privileged as they sat down to write about the problems faced by the rural population, putting themselves in their shoes.</p>
+                </div>
+                <div class="col-lg-5 col-md-6"><img class="img-responsive" src="/img/events/photojournalism.jpg"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-text-green">Painting Competition</h2></div>
+                <div class="col-lg-5 col-md-6"><img class="img-responsive" src="/img/events/book-donation.jpg"></div>
+                <div class="col-lg-7 col-md-6">
+                    <blockquote>Art enables us to find ourselves and lose ourselves at the same time.</blockquote>
+                    <p>With pencils, colors, papers DRISTI  conducted a drawing competition at Panchauli village for kids. The theme for painting was Health and Sanitation aiming to make them aware of the same. With the drawing children with further asked to make dustbins for their locality by pasting the drawing on cardboard. Children were further given the responsibility to be the caretaker of their own dustbins and dump all waste in them</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-text-green">Musical Classes</h2></div>
+                <div class="col-lg-7 col-md-6">
+                    <blockquote>Music doesn't lie. If there is something to be changed in this world, then it can only happen through music.</blockquote>
+                    <p>Major research and studies after studies have shown that learning music can make kids smart. When your child learns to play a musical instrument, not only does he learn how to make tunes, but he also enhances other capabilities of his brain as well.learning to play music develops discipline that is beneficial to academic achievement.</p>
+                    <p>And so members along with talented volunteers conducted Musical classes at Kislaya Vidya Mandir to teach the basics of music to school kids.</p>
+                </div>
+                <div class="col-lg-5 col-md-6"><img class="img-responsive" src="/img/events/musical-classes.jpg"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endblock() ?>
