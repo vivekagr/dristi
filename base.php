e03ccd2eb9d509f2e5d965890d438c519b6cdd2a
Vivek Agarwal <me@vivek.im>
2013-12-30 23:36:36 +0530

    Increase logo and nav bar links size.

--- base.php
+++ base.php
@@ -28,7 +28,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/"><img class="brand-logo" src="/img/logo.png" alt="Dristi Logo"> </a>
+                <a class="navbar-brand" href="/"><img class="brand-logo" src="/img/logo-large.png" alt="Dristi Logo"> </a>
             </div>
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav navbar-right site-nav">
