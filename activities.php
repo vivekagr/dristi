16d01229f99d1b0a82858cfd7d99d0105c1826e0
Vivek Agarwal <me@vivek.im>
2014-01-02 20:52:38 +0530

    Add 3 more activities.

--- activities.php
+++ activities.php
@@ -8,6 +8,18 @@
         <div class="col-md-12 full-width-block">
             <h1 class="heading-text">Activities</h1>
             <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-block">Tuition Classes</h2></div>
+                <div class="col-lg-10 col-md-9 col-sm-8">
+                    <p>TFree of cost education does not guarantee education. DRISTI knew the importance of individual attention for a holistic learning approach. Providing this interface to the rural kids helps them learn and grow like their urban counterparts. Main targets of the tuition classes are –</p>
+                    <ul>
+                        <li>Admission of students in class 6th entrance exam of Jawahar Navodaya Vidyalaya.</li>
+                        <li>Finding and funding the talented children in the village to complete his/her higher education in schools or colleges of repute.</li>
+                        <li>Letting them learn with the fun using educational tools,laptops, experiments,etc.</li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-4 hidden-xs"><img class="img-responsive" src="/img/activities/tuition-classes.jpg"></div>
+            </div>
+            <div class="row">
                 <div class="col-md-12"><h2 class="heading-text heading-block">Basic Computer Class</h2></div>
                 <div class="col-lg-10 col-md-9 col-sm-8">
                     <p>Through DRISTI we are trying to increase computer literacy amongst the villagers. We aim to develop in them a range of skills covering -</p>
@@ -34,6 +46,19 @@
                 <div class="col-lg-2 col-md-3 col-sm-4 hidden-xs"><img class="img-responsive" src="/img/activities/english-sessions.jpg"></div>
             </div>
             <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-block">Football Club - F.C. DRISTI</h2></div>
+                <div class="col-lg-10 col-md-9 col-sm-8">
+                    <p>The passion that girls of Jharkhand have for this game is nowhere to be seen in in other parts of India. The very own football team members of BIT Mesra are now the personal coaches for these girls.The main objectives of F.C. are -</p>
+                    <ul>
+                        <li>Mentoring them to work for the overall development in the game.</li>
+                        <li>Providing the girls of the  village to take up the game as a career.</li>
+                        <li>Providing assistance to girls who have been socially denied to choose a career for themselves in the society they are living in.</li>
+                        <li>Funding for the football kit.</li>
+                    </ul>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-4 hidden-xs"><img class="img-responsive" src="/img/activities/football.jpg"></div>
+            </div>
+            <div class="row">
                 <div class="col-md-12"><h2 class="heading-text heading-block">Career Counselling</h2></div>
                 <div class="col-lg-10 col-md-9 col-sm-8">
                     <p>You can teach a student a lesson for a day; but if you can teach him to learn by creating curiosity, he will continue the learning process as long as he lives. Member of DRISTI provide all kinds of support to youngster of villages towards career -</p>
@@ -58,6 +83,13 @@
                     <p>A session was conducted specially for women of village, Panchauli to identify and discuss the problems they face in everyday affair. The motto of the session was “You educate a man; you educate a man. You educate a woman; you educate a generation.” Many informational videos on pregnancy, malnutrition and general health care were shown to them. We also interacted with the local Anganwadi workers to get a better insight into its functioning.</p>
                 </div>
                 <div class="col-lg-2 col-md-3 col-sm-4 hidden-xs"><img class="img-responsive" src="/img/activities/women-issues.jpg"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-12"><h2 class="heading-text heading-block">The Night Trip</h2></div>
+                <div class="col-lg-10 col-md-9 col-sm-8">
+                    <p>Realizing the importance of visual media in information sharing ,Team DRISTI came up with this unique idea of showing films/educational videos to the villagers at night. This session involves learning with watching. All the learning is done with videos projected on screens and thereafter a valued lecture on social issues. So far numerous sessions have been conducted for villages Panchauli and Masu. The outcome has only motivated us to provide the villagers with more of such tutorials.</p>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-4 hidden-xs"><img class="img-responsive" src="/img/activities/night-trip.jpg"></div>
             </div>
         </div>
     </div>
