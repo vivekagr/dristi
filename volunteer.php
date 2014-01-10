eb83004a400a0780db63f35909c2c5c8997d0cbf
Vivek Agarwal <me@vivek.im>
2014-01-10 16:56:20 +0530

    Add more volunteer voices and change the layout a bit.

--- volunteer.php
+++ volunteer.php
@@ -12,35 +12,79 @@
             <p>Any time you can spare can change many lives. Your action your voice your conviction can turn the situation around for millions. It can inspire others to work for your cause too. ﻿DRISTI began as an endeavor of 7-8 young volunteers in February 2013 and ever since, the spirit of volunteerism has been an underlying lifeline of the organisation. For us, volunteers like you are a repository of energy, creativity and commitment. They are advocates who empower communities to bring change in their lives and through your willingness to give so much of yourself, keep us going on our path towards ensuring a lasting change.</p>
 </div>
 <div class="padded-container grey-container">
-    <div class="row">
-        <div class="col-md-6">
-            <h1 class="heading-text heading-text-green text-center">Volunteer Voices</h1>
-            <div class="row">
-                <div class="col-md-12 col-sm-6">
-                    <div class="row volunteer-voice">
-                        <div class="col-xs-3 col-xs-4"><img class="img-circle img-responsive" src="/img/volunteer/chetan.jpg"></div>
-                        <div class="col-xs-9 col-xs-8">
-                            <blockquote>
-                                I am working for a noble cause....... and i am loving it. it gives me happiness. and i have to thank u Utsav Nepal Vikas Pandey and all the members in DRISTI. keep inspiring people.
-                                <small>Chetan Koirala</small>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12 col-sm-6">
-                    <div class="row volunteer-voice">
-                        <div class="col-xs-3"><img class="img-circle img-responsive" src="/img/volunteer/wasim.jpg"></div>
-                        <div class="col-xs-9">
-                            <blockquote>
-                                d best part of DRISTI is that u work here with ur heart widout thinking abt single benefit but in return really you got a unconditional love & respect from your team n all d people especially kids..! thnx guyz for a unforgettable day....!
-                                <small>Wasim Akram</small>
-                            </blockquote>
-                        </div>
-                    </div>
+    <h1 class="heading-text heading-text-green text-center">Volunteer Voices</h1>
+    <div class="row volunteer-voices">
+        <div class="col-sm-6">
+            <div class="row volunteer-voice">
+                <div class="col-xs-3 col-xs-4"><img class="img-circle img-responsive" src="/img/volunteer/chetan.jpg"></div>
+                <div class="col-xs-9 col-xs-8">
+                    <blockquote>
+                        I am working for a noble cause....... and i am loving it. it gives me happiness. and i have to thank u Utsav Nepal Vikas Pandey and all the members in DRISTI. keep inspiring people.
+                        <small><a href="https://www.facebook.com/chetanskm">Chetan Koirala</a></small>
+                    </blockquote>
                 </div>
             </div>
         </div>
-        <div class="col-md-5 col-md-offset-1">
+        <div class="col-sm-6">
+            <div class="row volunteer-voice">
+                <div class="col-xs-3"><img class="img-circle img-responsive" src="/img/volunteer/wasim.jpg"></div>
+                <div class="col-xs-9">
+                    <blockquote>
+                        d best part of DRISTI is that u work here with ur heart widout thinking abt single benefit but in return really you got a unconditional love & respect from your team n all d people especially kids..! thnx guyz for a unforgettable day....!
+                        <small><a href="https://www.facebook.com/wasim.akram00">Wasim Akram</a></small>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="row volunteer-voice">
+                <div class="col-xs-3"><img class="img-circle img-responsive" src="/img/volunteer/ezaz.jpg"></div>
+                <div class="col-xs-9">
+                    <blockquote>
+                        Being students, although we want to see great changes around us, we are always left with very few resources with us. Dristi has been one such medium that has made me materialize my thoughts for the society. It has helped me return my bit to this land. It feels wonderful when I actually see smiling faces at deserted places and knowing its repercussions satisfies me even more.
+                        <small><a href="https://www.facebook.com/azsezaz">Ezaz</a></small>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="row volunteer-voice">
+                <div class="col-xs-3"><img class="img-circle img-responsive" src="/img/volunteer/anupama.jpg"></div>
+                <div class="col-xs-9">
+                    <blockquote>
+                        I believe in the power of social and technological integration for the upliftment of rural India. I possess the capability to transform rural Jharkhand into a collection of model villages. I spread hope and happiness in the hearts of numerous rural poor. I am utilsing my progressive outlook to create an understanding for opportunities in the minds of the poeple I work with. At the culmination of each work drive, I ensure that the smile passes on from myself to the faces of rural children.  I work for the transformation of rural Jharkhand. I am a DRISTI'an!
+                        <small><a href="https://www.facebook.com/anupama.sharan.98">Anupama Sharan</a></small>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="row volunteer-voice">
+                <div class="col-xs-3"><img class="img-circle img-responsive" src="/img/volunteer/sambhavi.jpg"></div>
+                <div class="col-xs-9">
+                    <blockquote>
+                        Sometimes when we are tired and look back at our journey of life, view our achievements in the rat's race,it's work like these that make us stand out  and be proud of our self...i hope the spirit of Dristi continues to acquire greater goals and the be successful at fulfilling them.
+                        <small><a href="https://www.facebook.com/sambhavi.priyadarshini">Sambhavi Priyadarshini</a></small>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="row volunteer-voice">
+                <div class="col-xs-3"><img class="img-circle img-responsive" src="/img/volunteer/abhimanyu.jpg"></div>
+                <div class="col-xs-9">
+                    <blockquote>
+                        Having been invited by a group of seniors, and on the insistence of a friend, I went for a meeting organised by DRISTI to lay down the very foundations of the group, and decide upon the course of action that DRISTI would aim to perform. Initially, all of us, whether a first year junior or a 4th year senior, kept his/her point across, and these points, after great analysis and scrutiny, were taken up and put into practice. We started going to Masu, a village around 2.5 kms away from BIT campus, and after conducting sessions related to topics of rural importance like sanitation, alcoholism and health education, we felt motivated to participate more actively and work for the cause with all our heart.
+                        <small><a href="https://www.facebook.com/abhimanyu.karandikar">Abhimanyu Karandikar</a></small>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="padded-container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
             <h1 class="heading-text heading-text-green text-center">Interested In Volunteering For DRISTI?</h1>
             <form class="volunteer-form" role="form" method="post" action="contact.php">
                 <div class="form-group">
@@ -61,5 +105,6 @@
             </form>
         </div>
     </div>
+</div>
 </div>
 <?php endblock() ?>
